b5142f41caf29e336596587fcfe8c7b363c186ca
karim Med <mr.medar.karim@gmail.com>
2023-01-09 14:41:33 +0100

    update ex13

--- 13_clickHandler.js
+++ 13_clickHandler.js
@@ -2,8 +2,7 @@ let btn = document.querySelector("button");
 btn.addEventListener("click", (e) => {
   let content = document.querySelector(".content");
   let p = document.createElement("p");
+  p.setAttribute("id", "result");
   p.textContent = "clicked";
   content.appendChild(p);
-  console.log(content);
-  console.log(p);
 });
