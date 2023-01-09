ed1ac6e6134af275dc133b64fa7bebd737b27d1e
karim Med <mr.medar.karim@gmail.com>
2023-01-09 16:16:10 +0100

    add ex16

---
+++ 16_contactForm.js
@@ -0,0 +1,10 @@
+btn = document.querySelector("button");
+
+btn.addEventListener("click", (e) => {
+  let inputs = Array.from(document.querySelectorAll("input"));
+  let body = document.body;
+  newP = document.createElement("p");
+  newP.textContent = `${inputs[0].name}: ${inputs[0].value}, 
+  ${inputs[1].name}: ${inputs[1].value}, ${inputs[2].name}: ${inputs[2].value}`;
+  body.append(newP);
+});
