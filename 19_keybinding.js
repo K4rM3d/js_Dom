479aa6a543fc6cad78411c047da1a3d4c621f283
karim Med <mr.medar.karim@gmail.com>
2023-01-10 12:15:57 +0100

    update ex19

--- 19_keybinding.js
+++ 19_keybinding.js
@@ -1,10 +1,12 @@
 document.addEventListener("keydown", (e) => {
   let square = document.querySelector("#square");
 
+  // Recupere la valeur de la possition top du square en entier
   let squareTop = square.style.top ? square.style.top : "0px";
   squareTop = squareTop.replace("px", "");
   squareTop = parseInt(squareTop);
 
+  // Recupere la valeur de la possition left du square en entier
   let squareLeft = square.style.left ? square.style.left : "0px";
   squareLeft = squareLeft.replace("px", "");
   squareLeft = parseInt(squareLeft);
