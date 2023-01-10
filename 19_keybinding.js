949cb3fae75a6a7b8a0c06cbc985a9b639105e8d
karim Med <mr.medar.karim@gmail.com>
2023-01-10 11:39:38 +0100

    update ex19

--- 19_keybinding.js
+++ 19_keybinding.js
@@ -1,37 +1,27 @@
 document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowDown") {
-    let square = document.querySelector("#square");
-    let squareTop = square.style.top ? square.style.top : "0px";
+  let square = document.querySelector("#square");
 
-    squareTop = squareTop.replace("px", "");
-    squareTop = parseInt(squareTop);
+  let squareTop = square.style.top ? square.style.top : "0px";
+  squareTop = squareTop.replace("px", "");
+  squareTop = parseInt(squareTop);
+
+  let squareLeft = square.style.left ? square.style.left : "0px";
+  squareLeft = squareLeft.replace("px", "");
+  squareLeft = parseInt(squareLeft);
+
+  if (e.key === "ArrowDown") {
     square.style.top = `${squareTop + 10}px`;
   }
 
   if (e.key === "ArrowUp") {
-    let square = document.querySelector("#square");
-    let squareTop = square.style.top ? square.style.top : "0px";
-
-    squareTop = squareTop.replace("px", "");
-    squareTop = parseInt(squareTop);
     square.style.top = `${squareTop - 10}px`;
   }
 
   if (e.key === "ArrowRight") {
-    let square = document.querySelector("#square");
-    let squareLeft = square.style.left ? square.style.left : "0px";
-
-    squareLeft = squareLeft.replace("px", "");
-    squareLeft = parseInt(squareLeft);
     square.style.left = `${squareLeft + 10}px`;
   }
 
   if (e.key === "ArrowLeft") {
-    let square = document.querySelector("#square");
-    let squareLeft = square.style.left ? square.style.left : "0px";
-
-    squareLeft = squareLeft.replace("px", "");
-    squareLeft = parseInt(squareLeft);
     square.style.left = `${squareLeft - 10}px`;
   }
 });
