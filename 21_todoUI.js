6b4e6b5c728ae2e873cff536d8a46c08685dab5e
karim Med <mr.medar.karim@gmail.com>
2023-01-10 15:53:29 +0100

    update ex21

--- 21_todoUI.js
+++ 21_todoUI.js
@@ -3,9 +3,8 @@ let btn = document.querySelector("#addTodo");
 btn.addEventListener("click", (e) => {
   let input = document.querySelector("input");
   let list = document.querySelector("ul");
-  console.log(list);
   let task = document.createElement("li");
+
   task.textContent = `${input.value}`;
-  console.log(input.value);
   list.appendChild(task);
 });
