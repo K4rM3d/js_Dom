4e966e057a75ee34674835254aa7f424b5a47176
karim Med <mr.medar.karim@gmail.com>
2023-01-11 11:51:47 +0100

    add ex21

--- 21_todoUI.js
+++ 21_todoUI.js
@@ -4,7 +4,16 @@ btn.addEventListener("click", (e) => {
   let input = document.querySelector("input");
   let list = document.querySelector("ul");
   let task = document.createElement("li");
+  let btnDelete = document.createElement("button");
 
   task.textContent = `${input.value}`;
+  btnDelete.textContent = "Delete";
   list.appendChild(task);
+  list.appendChild(btnDelete);
+  console.log(list);
+
+  btnDelete.addEventListener("click", (e) => {
+    btnDelete.previousElementSibling.remove();
+    btnDelete.remove();
+  });
 });
