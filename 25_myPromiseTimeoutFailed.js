24b207e37e11c75780104faf5f390138b97c4141
karim Med <mr.medar.karim@gmail.com>
2023-01-12 12:13:20 +0100

    add ex25

---
+++ 25_myPromiseTimeoutFailed.js
@@ -0,0 +1,13 @@
+const sleepThrow = (millisecondes, condition) => {
+  return new Promise((resolve, reject) => {
+    if (condition === true) {
+      setTimeout(() => resolve("Success"), millisecondes);
+    } else {
+      setTimeout(() => reject("Failed"), millisecondes);
+    }
+  });
+};
+
+sleepThrow(2000, true)
+  .then(() => console.log("ok"))
+  .catch(() => console.log("non"));
